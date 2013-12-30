16f5d350c9ba70d5a8afffa441b35997bc04b1c4
lnxslackware <lnxslackware@gmail.com>
2013-12-30 21:40:55 +0200

    C# 2 - Edited Project 4 from Solution 2
    Edited Project 4 - Binary Search - simplify version

--- C# part 2/02. Multidimensional-Arrays/04. BinarySearch/BinarySearch.cs
+++ C# part 2/02. Multidimensional-Arrays/04. BinarySearch/BinarySearch.cs
@@ -9,34 +9,32 @@ class BinarySearch
     static void Main()
     {
         //Read the user input from the console and initialize the array
-        Console.WriteLine("Enter N(count of elements in the array): ");
+        Console.Write("Enter N(count of elements in the array): ");
         int n = int.Parse(Console.ReadLine());
         int[] numbers = new int[n];
         Console.WriteLine("Enter array elements separated with a single space:");
         string userInput = Console.ReadLine();
         string[] userInputAsArray = userInput.Split(' ');
-        int resultIndex = -1;
 
         for (int i = 0; i < n; i++)
         {
             numbers[i] = int.Parse(userInputAsArray[i]);
         }
+
         Console.Write("Enter K: ");
         int k = int.Parse(Console.ReadLine());
 
         //Sorting the array
         Array.Sort(numbers);
 
+        //Getting the result
+        int resultIndex = Array.BinarySearch(numbers, k);
+
         //Finding element equal or nearest not grater than K
 
-        for (int i = k; i >= 0; i--)
+        if (resultIndex < 0)
         {
-            resultIndex = Array.BinarySearch(numbers, i);
-            if (resultIndex > -1)
-            {
-                break;
-            }
-            
+            resultIndex = ~resultIndex - 1;
         }
 
         Console.WriteLine("The largest number in the array which is less or equal to \"K\" is with index {0} and has value {1}", resultIndex, numbers[resultIndex]);
