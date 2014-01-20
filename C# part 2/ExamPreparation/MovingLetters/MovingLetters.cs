ec969591affa6c2eca6630191d60575e25fb37d2
lnxslackware <lnxslackware@gmail.com>
2014-01-20 22:31:54 +0200

    C# 2 - Exam Preparation

---
+++ C# part 2/ExamPreparation/MovingLetters/MovingLetters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+class MovingLetters
+{
+
+    static void Main()
+    {
+        string[] inputWords = Console.ReadLine().Split(' ');
+
+        //getting the longest word's length
+        int longestWordLength = 0;
+
+        foreach (var word in inputWords)
+        {
+            if (longestWordLength < word.Length)
+            {
+                longestWordLength = word.Length;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        //List<char> chars = new List<char>();
+
+        //adding all chars to the list
+        for (int i = 0; i < longestWordLength; i++)
+        {
+            foreach (var word in inputWords)
+            {
+                if (word.Length > i)
+                {
+                    sb.Append(word[word.Length - 1 - i]);
+                }
+            }
+        }
+
+        //moving the words
+        for (int i = 0; i < sb.Length; i++)
+        {
+            int steps = char.ToLower(sb[i]) - '`';
+            //Console.WriteLine("{0}", steps);
+            int newIndex = (steps + i) % sb.Length;
+
+            char tmp = sb[i];
+            sb.Remove(i, 1);
+            sb.Insert(newIndex, tmp);
+
+            //if (newIndex > i)
+            //{
+            //}
+            //else
+            //{
+            //    chars.RemoveAt(i - 1);
+            //}
+        }
+
+        Console.WriteLine(sb);
+
+    }
+}
---
+++ C# part 2/ExamPreparation/Zerg/Zerg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+class Zerg
+{
+    static BigInteger calculatePow(int index)
+    {
+        if (index == 0)
+        {
+            return 1;
+        }
+
+        BigInteger pow = 1;
+
+        for (int i = 0; i < index; i++)
+        {
+            pow *= 15; 
+        }
+
+        return pow;
+    }
+
+    static void Main()
+    {
+        string zergMessage = Console.ReadLine();
+        string wordsPattern = @"[A-Z][a-z]{3}";
+        string[] dictionary = { "Rawr", "Rrrr", "Hsst", "Ssst", "Grrr", "Rarr", "Mrrr", "Psst", "Uaah", "Uaha", "Zzzz", "Bauu", "Djav", "Myau", "Gruh" };
+
+        MatchCollection words = Regex.Matches(zergMessage, wordsPattern);
+
+        BigInteger answer = 0;
+
+        for (int i = words.Count - 1, j = 0; i >= 0; i--, j++)
+        {
+            int value = Array.IndexOf(dictionary, words[i].ToString());
+            answer += (BigInteger)value * calculatePow(j);
+        }
+
+        Console.WriteLine(answer);
+    }
+}
